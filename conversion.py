0eb73429b6661da26b038bbdf4a3370b9b33c7c0
Nma007 <iokiyi@yahoo.com>
2024-12-18 21:26:14 +0000

    Add files via upload

---
+++ conversion.py
@@ -0,0 +1,39 @@
+#Assigning different integers to variables num1, num2, num3
+#Assign 100 to the variable string1 using ("") making it a string
+
+num1 = 99.23
+num2 = 23
+num3 = 150
+string1 = "100"
+
+#Converting variables from a particular data type into a different data type
+#Convert num1 to an integer
+#num2 to a float
+#num3 to a string
+#string1 to an integer
+#Print out all variables
+
+#Convert num1 into an integer
+num1 = 99.23
+num_int = int(num1)
+print("The number is: ")
+print(num_int)
+
+#Convert num2 into a float
+num2 = 23
+num_float = float(num2)
+print("The float is: ")
+print(num_float)
+
+#Convert num3 to a string
+num3 = 150
+num_string = str(num3)
+print("The string is: ")
+print(num_string)
+
+#Convert string1 to an integer
+string1 = "100"
+number_int = int(string1)
+print("This becomes: ")
+print(number_int)
+
