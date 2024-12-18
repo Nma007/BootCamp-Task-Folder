2f7316ce8aa3ce949bfd90765afc9b5da453da63
Nma007 <iokiyi@yahoo.com>
2024-12-18 14:40:09 +0000

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+#Request input from user, store input into variable called user_name then print output "user_name"
+#Request input from user, store input in variable called user_age then output "user_age"
+#Using the print command print 'Hello World' in a new line
+
+user_name = input("Enter Name: ")
+user_age = input("Enter Age: ")
+print (user_name, user_age)
+print ("You, {}, is {} years old!".format (user_name, user_age))
+
+#Using the print command for Hello World
+print ("Hello World!")
